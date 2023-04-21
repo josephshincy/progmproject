68d37412f105b12aa8b2c96281b4239ea258f471
josephshincy <shincymalisseril98@gmail.com>
2023-04-21 15:35:39 -0600

    deleting comment for creating pie chart

--- PY5.py
+++ PY5.py
@@ -1,11 +1,2 @@
-from PY1 import deaths_df, confirmed_cases_df
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-global_confirmed_cases = confirmed_cases_df.iloc[:, -1].sum()
-global_deaths = deaths_df.iloc[:, -1].sum()
 
-# Create a dataframe with the global totals
-global_totals_df = pd.DataFrame({
-    "Confirmed Cases": [global_confirmed_cases]
 
