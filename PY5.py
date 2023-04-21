10cc6ffc031ce1193bce2d4f1f077a5c73f5f1c1
josephshincy <shincymalisseril98@gmail.com>
2023-04-21 15:34:21 -0600

    comment for creating pie chart

--- PY5.py
+++ PY5.py
@@ -1,11 +1,11 @@
-from PY2 import confirmed_cases, covid_deaths
+from PY1 import deaths_df, confirmed_cases_df
 import matplotlib.pyplot as plt
-# Create bar chart
-plt.bar(confirmed_cases, covid_deaths)
-# Customize chart appearance
-plt.xlabel('Confirmed_cases')  # Set x-axis label
-plt.ylabel('covid_deaths')  # Set y-axis label
-plt.title('Bar Chart ')  # Set chart title
+import pandas as pd
+import numpy as np
+global_confirmed_cases = confirmed_cases_df.iloc[:, -1].sum()
+global_deaths = deaths_df.iloc[:, -1].sum()
 
-# Show chart
-plt.show()
+# Create a dataframe with the global totals
+global_totals_df = pd.DataFrame({
+    "Confirmed Cases": [global_confirmed_cases]
+
