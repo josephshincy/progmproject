704950fe0a926b4fb6ccd25af30061a8353657c5
wright7889-spt <wright7889@saskpolytech.ca>
2023-04-21 15:48:05 -0600

    add in PY5 to plot time series of stock data

--- PY5.py
+++ PY5.py
@@ -1,2 +1,23 @@
+#Visualizations
 
+#first check that you have installed packages pandas and matplotlib
+import pandas as pd
+import matplotlib as plt
+from PY4 import stocks_covid
+
+#plot line time series of all
+fig, ax = plt.subplots(5,figsize = (15,12))
+stock_columns = stocks_covid.columns[2::]
+i=0
+r=0
+c=0
+for x in stock_columns:
+  ax[r].plot(stocks_covid.index, stocks_covid[x], label=x)
+  ax[r].legend(bbox_to_anchor = (1,0.3))
+  ax[r].set_ylabel('Price ($)')
+  if i%2 != 0:
+    r+=1
+  i+=1
+fig.suptitle('Stock Daily High/Low over Time')
+fig.tight_layout()
 
